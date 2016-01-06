809d06b20bfebea6487f1cb873d9023c743b6799
BEPIS05 <BEPIS05>
2016-01-06 17:19:48 -0500

    gulp install

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function () {
+  gulp.src('./scss/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+                    browsers: ['last 5 versions','> 1%',
+                        'ie 8',
+                        'ie 9',
+                        'ios 6',
+                        'android 4'],
+                    cascade: true
+                }))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+
+
+gulp.task('default', function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./scss/styles.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
